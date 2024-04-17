7904f9f5d33e8e18ed64bef47f39bc18abee9b7b
glassk <glassk0724@gmail.com>
2024-04-18 07:46:20 +0900

    [chore] enhance ESLint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  ignorePatterns: [
-    'dist',
-    '.eslintrc.cjs',
-    '.lintstagedrc.cjs',
-    'commitlint.config.cjs',
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["dist", ".eslintrc.cjs", ".lintstagedrc.cjs", "commitlint.config.cjs"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['prettier'],
+  plugins: ["@typescript-eslint", "prettier"],
 };
